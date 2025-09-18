e0398207548d63d70cfcb9673a0b0b6a2de38e6b
zumer305 <zumerniaz305@gmail.com>
2025-09-19 01:17:25 +0500

    push

---
+++ app.js
@@ -0,0 +1,60 @@
+// window object 
+// console.log(document);    (Thora HTML,CSS)
+// console.dir(document);     (long code javascript)
+
+
+// console.dir(document.all[6].innertext);       all-sab a jaty       [6]-index ma h1 para  innerText-value
+
+
+
+//getElementsById ............................................................................
+             // mainImg,srctagName (reserved words)         
+// document.getElementById("mainImg");    mainImg-id name
+// let imgObj=document.getElementById("mainImg");
+// imgObj.src
+// imgObj.tagName
+
+
+
+// getElementcByClassName.....................................................................
+// document.getElementByClassName("oldImg");
+// document.getElementByClassName("oldImg")[0]; class ma zayada objects hoty to is wjh sa [0],[1]indexes aty
+
+
+
+// getElementsByTagName...........................................................................
+// document.getElementBytagName("p");
+// document.getElementBytagName("p")[1].innerText;     0-tagname   1-description    
+
+   
+// -------------------------------------------------------------------------------------------------------------
+
+
+
+// QERY SELECTOR 
+// document.querySelector("p");   For tags
+// document.querySelector("#Id");   For ID
+// document.querySelector(".class");   For class 
+
+// document.querySelectorAll("p");   For all 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
